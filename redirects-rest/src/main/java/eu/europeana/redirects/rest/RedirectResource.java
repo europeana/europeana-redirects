2bf6e811c64169b28bdfb42a549d4617dba70b5a
ymamakis <yorgos.mamakis@europeana.eu>
2016-04-06 11:43:37 +0200

    Minor modifications

--- redirects-rest/src/main/java/eu/europeana/redirects/rest/RedirectResource.java
+++ redirects-rest/src/main/java/eu/europeana/redirects/rest/RedirectResource.java
@@ -1,12 +1,10 @@
 package eu.europeana.redirects.rest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import eu.europeana.redirects.model.RedirectRequest;
 import eu.europeana.redirects.model.RedirectRequestList;
 import eu.europeana.redirects.model.RedirectResponse;
 import eu.europeana.redirects.model.RedirectResponseList;
 import eu.europeana.redirects.service.RedirectService;
-import eu.europeana.redirects.service.mongo.MongoRedirectService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
@@ -15,7 +13,6 @@ import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.io.IOException;
 
 /**
  * REST Endpoint for Europeana Redirects Service module
@@ -33,13 +30,9 @@ public class RedirectResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     @ApiOperation(value="Generate a single redirect",response = RedirectResponse.class)
-    public Response redirectSingle(@ApiParam("record") @FormParam("record")String request){
-        try {
-            return Response.ok().entity(redirectService.createRedirect(new ObjectMapper().readValue(request, RedirectRequest.class))).build();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return null;
+    public Response redirectSingle(@ApiParam("record") @FormParam("record")RedirectRequest request){
+            return Response.ok().entity(redirectService.createRedirect(request)).build();
+
     }
     @POST
     @Path("/redirect/batch")
